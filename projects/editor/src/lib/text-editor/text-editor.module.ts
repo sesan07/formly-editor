80fde07c8c4050f6074f4c4418a767ab44f239e4
Samuel Esan <sesan@upei.ca>
2023-01-29 11:46:53 -0400

    feat(text-editor): add text editor with codemirror

---
+++ projects/editor/src/lib/text-editor/text-editor.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { TextEditorComponent } from './text-editor.component';
+
+@NgModule({
+    declarations: [TextEditorComponent],
+    exports: [TextEditorComponent],
+})
+export class TextEditorModule {}
