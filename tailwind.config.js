220184f3b0a8f4a88705d58acf2764d5aca1947c
Samuel Esan <sesan@upei.ca>
2023-02-28 20:50:37 -0400

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,92 +1,28 @@
-var breakpoints = ['sm', 'md', 'lg', 'xl'];
+// Plugins
+const layout = [
+    'display',
+    'flexDirection',
+    'gap',
+    'gridColumn',
+    'gridColumnEnd',
+    'gridColumnStart',
+    'gridRow',
+    'gridRowEnd',
+    'gridRowStart',
+    'gridTemplateColumns',
+    'gridTemplateRows',
+];
+const spacing = ['padding', 'margin'];
+const sizing = ['width', 'height'];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     safelist: [
-        'grid',
-        'flex',
         {
-            // pattern: /bg-(red|green|blue)-(100|200|300)/,
-            pattern: /col-(start|span)-\d+/,
-            variants: breakpoints, // you can add your variants here
-        },
-        {
-            pattern: /row-(start|span)-\d+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /grid-(cols|rows)-\d+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /gap-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /w-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /h-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /m-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /mx-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /my-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /mt-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /mr-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /mb-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /ml-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /p-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /px-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /py-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /pt-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /pr-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /pb-\S+/,
-            variants: breakpoints,
-        },
-        {
-            pattern: /pl-\S+/,
-            variants: breakpoints,
+            pattern: /./,
+            variants: ['sm', 'md', 'lg', 'xl'],
         },
     ],
-    content: [],
     theme: {
         screens: {
             sm: '640px',
@@ -94,14 +30,6 @@ module.exports = {
             lg: '1024px',
             xl: '1280px',
         },
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-        },
-        extend: {},
     },
-    plugins: [],
-    corePlugins: {
-        preflight: false, // Disable preflight to avoid unexpected styling differences
-    },
+    corePlugins: [...layout, ...spacing, ...sizing],
 };
