e469d61950e5ad12c1b31fa28b9b4e73181a6c8f
Samuel Esan <samuel.esan@proton.me>
2023-10-16 23:59:04 -0300

    chore: remove complex properties from public api

--- projects/editor/src/public-api.ts
+++ projects/editor/src/public-api.ts
@@ -5,11 +5,9 @@
 export { IBaseProperty, IProperty, PropertyType } from './lib/property/property.types';
 export { IArrayProperty } from './lib/property/object-array-properties/array-property.types';
 export { IBooleanProperty } from './lib/property/boolean-property/boolean-property.types';
-export { IChipListProperty } from './lib/property/chip-list-property/chip-list-property.types';
 export { IInputProperty } from './lib/property/input-property/input-property.types';
 export { ITextareaProperty } from './lib/property/textarea-property/textarea-property.types';
 export { IObjectProperty } from './lib/property/object-array-properties/object-property.types';
-export { IExpressionPropertiesProperty } from './lib/property/expression-properties-property/expression-properties-property.types';
 
 export { EditorComponent } from './lib/editor.component';
 export { FormlyGroupComponent } from './lib/custom-formly/formly-group/formly-group.component';
