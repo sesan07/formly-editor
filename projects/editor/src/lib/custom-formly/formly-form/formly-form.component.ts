d6f2233be972f9e965c2f36b3e0ee8cc58689407
Samuel Esan <samuel.esan@proton.me>
2023-12-16 13:28:07 -0400

    feat(formly-form): catch and show build errors

--- projects/editor/src/lib/custom-formly/formly-form/formly-form.component.ts
+++ projects/editor/src/lib/custom-formly/formly-form/formly-form.component.ts
@@ -1,18 +1,19 @@
-import { ChangeDetectionStrategy, Component, NgZone } from '@angular/core';
+import { ChangeDetectionStrategy, Component, NgZone, SimpleChanges } from '@angular/core';
 import { FormlyConfig, FormlyForm, FormlyFormBuilder } from '@ngx-formly/core';
+import { BehaviorSubject } from 'rxjs';
 
 import { EditorFieldType, IEditorFormlyFieldConfigCache } from '../../editor.types';
 import { FormlyFieldTemplates } from '../formly.template';
 
 @Component({
     selector: 'editor-formly-form',
-    template: '<editor-root-formly-field [field]="field"></editor-root-formly-field>',
+    templateUrl: './formly-form.component.html',
     styleUrls: ['./formly-form.component.scss'],
     providers: [FormlyFormBuilder, FormlyFieldTemplates],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormlyFormComponent extends FormlyForm {
-    override field: IEditorFormlyFieldConfigCache = {
+    public override field: IEditorFormlyFieldConfigCache = {
         _info: {
             name: undefined,
             formId: undefined,
@@ -22,6 +23,8 @@ export class FormlyFormComponent extends FormlyForm {
         type: EditorFieldType.FORMLY_GROUP,
     };
 
+    public formError$: BehaviorSubject<string> = new BehaviorSubject('');
+
     constructor(
         builder: FormlyFormBuilder,
         config: FormlyConfig,
@@ -29,5 +32,16 @@ export class FormlyFormComponent extends FormlyForm {
         fieldTemplates: FormlyFieldTemplates
     ) {
         super(builder, config, ngZone, fieldTemplates);
+    }
+
+    override ngOnChanges(changes: SimpleChanges): void {
+        try {
+            super.ngOnChanges(changes);
+            this.formError$.next('');
+        } catch (e) {
+            this.field.fieldGroup = [];
+            this.formError$.next(`Build Error: ${e.message}`);
+            console.error(e);
+        }
     }
 }
