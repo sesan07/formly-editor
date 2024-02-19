5e250f1ad19af341beafcac1102c2b2cbc935a62
Samuel Esan <samuel.esan@proton.me>
2024-02-19 12:24:23 -0400

    refactor(properties): move service functions to utils

--- projects/editor/src/lib/property/utils.ts
+++ projects/editor/src/lib/property/utils.ts
@@ -3,7 +3,9 @@ import { get, set, unset } from 'lodash-es';
 import { IArrayProperty } from './cyclic-properties/array-property.types';
 import { IObjectProperty } from './cyclic-properties/object-property.types';
 import { IExpressionPropertiesProperty } from './expression-properties-property/expression-properties-property.types';
+import { IInputProperty } from './input-property/input-property.types';
 import { IBaseProperty, IProperty, IPropertyChange, PropertyChangeType, PropertyType } from './property.types';
+import { ITextareaProperty } from './textarea-property/textarea-property.types';
 
 export const initRootProperty = (
     property: IObjectProperty | IArrayProperty,
@@ -64,5 +66,93 @@ export const modifyPropertyTarget = <T extends Record<string, any>>(
             return modifyTargetKey(target, change);
         case PropertyChangeType.VALUE:
             return modifyTargetValue(target, change);
+    }
+};
+
+export const getDefaultProperty = (type: PropertyType, key: string | number = ''): IProperty => {
+    // Properties added by the user should have editable keys and be deletable
+    const baseProperty: IBaseProperty = {
+        type,
+        key,
+        isKeyEditable: true,
+        isRemovable: true,
+    };
+
+    switch (type) {
+        case PropertyType.BOOLEAN:
+        case PropertyType.TEXT:
+            return {
+                ...baseProperty,
+                outputRawValue: true,
+            };
+        case PropertyType.TEXTAREA:
+            return {
+                ...baseProperty,
+                minRows: 2,
+            } as ITextareaProperty;
+        case PropertyType.NUMBER:
+            return {
+                ...baseProperty,
+                outputRawValue: true,
+            } as IInputProperty;
+        case PropertyType.OBJECT:
+            return {
+                ...baseProperty,
+                canAdd: true,
+                childProperties: [],
+                populateChildrenFromTarget: true,
+            } as IObjectProperty;
+        case PropertyType.ARRAY:
+            return {
+                ...baseProperty,
+                canAdd: true,
+            } as IArrayProperty;
+    }
+};
+
+export const getDefaultPropertyFromValue = (value: any, key?: string): IProperty | undefined => {
+    const propertyType: PropertyType | undefined = getPropertyType(value);
+    if (!propertyType) {
+        return undefined;
+    }
+    return getDefaultProperty(propertyType, key);
+};
+
+export const getDefaultPropertyValue = (type: PropertyType): any => {
+    switch (type) {
+        case PropertyType.ARRAY:
+            return [];
+        case PropertyType.BOOLEAN:
+            return false;
+        case PropertyType.OBJECT:
+            return {};
+        case PropertyType.NUMBER:
+            return 0;
+        case PropertyType.TEXT:
+        case PropertyType.TEXTAREA:
+            return '';
+        default:
+            throw new Error(`Property type ${type} does not have a default value`);
+    }
+};
+
+const getPropertyType = (value: any): PropertyType | undefined => {
+    switch (typeof value) {
+        case 'object':
+            if (value === null) {
+                return undefined;
+            } else if (Array.isArray(value)) {
+                return PropertyType.ARRAY;
+            }
+
+            return PropertyType.OBJECT;
+        case 'boolean':
+            return PropertyType.BOOLEAN;
+        case 'number':
+            return PropertyType.NUMBER;
+        case 'string':
+            return PropertyType.TEXT;
+        default:
+            return undefined;
     }
 };
